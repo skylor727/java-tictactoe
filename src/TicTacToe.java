60aa3996dcc42977a253a4d922946d9430d11f49
skylor727 <skylor1997@hotmail.com>
2022-05-27 18:58:59 -0700

    Refactor game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,47 +1,97 @@
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter cells:");
-        String board = scanner.next().toUpperCase();
-        char[] boardArr = board.toCharArray();
+        int turn = 1;
+        String board = "_________";
+        printBoard(board);
+        char winner = checkWinner(board);
+        while (winner == 'Z' && !checkDraw(board)) {
+            String[] newVals = handleMove(board, turn);
+            board = newVals[0];
+            turn = Integer.parseInt(newVals[1]);
+            printBoard(board);
+            winner = checkWinner(board);
+        }
+
+        if (!checkDraw(board)) System.out.println(winner + " wins");
+        else System.out.println("Draw");
+    }
+
+
+    public static void printBoard(String board) {
         System.out.println("---------");
         System.out.println("| " + board.charAt(0) + " " + board.charAt(1) + " " + board.charAt(2) + " |");
         System.out.println("| " + board.charAt(3) + " " + board.charAt(4) + " " + board.charAt(5) + " |");
         System.out.println("| " + board.charAt(6) + " " + board.charAt(7) + " " + board.charAt(8) + " |");
         System.out.println("---------");
 
-        char winner = checkWinner(boardArr);
-        if (isImpossible(boardArr, winner)) System.out.println("Impossible");
-        if (!isFinished(boardArr) && (winner != 'X' && winner != 'O') && (!isImpossible(boardArr, winner)))
-            System.out.println("Game not finished");
-        if (isFinished(boardArr) && !isImpossible(boardArr, winner) && winner != 'X' && winner != 'O')
-            System.out.println("Draw");
-        if (winner == 'X') System.out.println("X wins");
-        if (winner == 'O') System.out.println("O wins");
     }
 
-    public static boolean isImpossible(char[] board, char winner) {
-        int differenceCtr = 0;
-        int xCtr = 0;
-        int circleCtr = 0;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i] == 'X') xCtr++;
-            if (board[i] == 'O') circleCtr++;
-            differenceCtr = xCtr - circleCtr;
+    public static String[] handleMove(String board, int turn) {
+        char[][] boardArr = new char[3][3];
+        int[] coordinates;
+        boolean validNums = false;
+        int ctr = 0;
+        int xCoord = 0;
+        int yCoord = 0;
+        //Break the string board into a 2d array
+        for (int i = 0; i < boardArr.length; i++) {
+            for (int x = 0; x < boardArr[i].length; x++) {
+                boardArr[i][x] = board.charAt(ctr);
+                ctr++;
+            }
         }
-        if (differenceCtr < 0) differenceCtr *= -1;
 
-        return (differenceCtr > 1 || winner == 'Z') ? true : false;
+        //Continue Looping for as long as the user enters invalid coordinates
+        do {
+            coordinates = getCoordinates(boardArr);
+            xCoord = coordinates[0] - 1;
+            yCoord = coordinates[1] - 1;
+            if (coordinates[0] >= 1 && coordinates[0] < 4 && coordinates[1] >= 1 && coordinates[1] < 4)
+                validNums = true;
+        } while (!validNums);
+
+        //Update board
+        boardArr[xCoord][yCoord] = turn == 1 ? 'X' : 'O';
+        turn *= -1;
+        String updatedBoard = "";
+        for (int i = 0; i < boardArr.length; i++) {
+            for (int x = 0; x < boardArr.length; x++) {
+                updatedBoard += boardArr[i][x];
+            }
+        }
+        String[] returnVals = new String[]{updatedBoard, String.valueOf(turn)};
+        return returnVals;
     }
 
-    public static boolean isFinished(char[] board) {
-        return new String(board).contains("_") || new String(board).contains(" ") ? false : true;
+    public static int[] getCoordinates(char[][] boardArr) {
+        Scanner scanner = new Scanner(System.in);
+        int[] coordinates = new int[2];
+        System.out.print("Enter the coordinates: ");
+        String[] coordinateString = (scanner.nextLine().split(" "));
+        try {
+            coordinates[0] = Integer.parseInt(coordinateString[0]);
+            coordinates[1] = Integer.parseInt(coordinateString[1]);
+            int xCoord = coordinates[0] - 1;
+            int yCoord = coordinates[1] - 1;
+            if (xCoord < 0 || xCoord > 2 || yCoord < 0 || yCoord > 2)
+                System.out.println("Coordinates should be from 1 to 3!");
+            else if (boardArr[xCoord][yCoord] == 'X' || boardArr[xCoord][yCoord] == 'O') {
+                System.out.println("This cell is occupied! Choose another one!");
+                coordinates[0] = -1;
+            }
+        } catch (Exception e) {
+            System.out.println("You should enter numbers");
+        }
+        return coordinates;
     }
 
-    public static char checkWinner(char[] board) {
+    public static char checkWinner(String boardStr) {
+        char board[] = boardStr.toCharArray();
         int[][] winningCombos = {
                 //Vertical
                 {2, 5, 8},
@@ -55,7 +105,8 @@ public class TicTacToe {
                 {2, 4, 6},
                 {0, 4, 8}
         };
-        int winnerCtr = 0;
+
+
         char[] winner1 = {'X', 'X', 'X'};
         char[] winner2 = {'O', 'O', 'O'};
         char winner = 0;
@@ -68,17 +119,18 @@ public class TicTacToe {
         for (int i = 0; i < twoDBoard.length; i++) {
             if (Arrays.equals(twoDBoard[i], winner1)) {
                 winner = 'X';
-                winnerCtr++;
             } else if (Arrays.equals(twoDBoard[i], winner2)) {
                 winner = 'O';
-                winnerCtr++;
             }
 
         }
+        return winner != 0 ? winner : 'Z';
+    }
 
-        return winnerCtr < 2 ? winner : 'Z';
+    public static boolean checkDraw(String board) {
+        if (board.contains("_") || board.contains(" ")) {
+            return false;
+        }
+        return checkWinner(board) == 'Z' ? true : false;
     }
 }
-
-
-
